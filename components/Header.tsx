64281404c6ee2f71a2c67588a039cbe55f31268e
highsilkhat <pl4400@protonmail.com>
2023-03-26 10:16:46 -0500

    working on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,78 @@
 import MenuLink from './MenuLink'
-import Newspaper from '../assets/newspaper.svg'
+import Mail from '../assets/mail.svg'
+import Link from 'next/link'
 
 const Header: React.VFC = () => {
     return(
         
+        
         <div
-            className="
+            className='
             py-10 md:py-20
-            font-sans
-            bg-red-500
-            "
+        '
         >
-            <MenuLink
-                href="/bio"
+
+            <div
                 className='
-                group-hover: text-blue-500
-                group-hover:text-opacity-100'
-                iconComponent={Newspaper}
-                iconText="newspaper"
-                iconTextStyling='
-                text-xs'
-            
-            />
+                text-center md:text-left
+                 bg-green-400
+                 bg-opacity-70 '
+            >
+                    <div
+                    className="
+                            flex-col md:flex-row
+                            flex justify-between
+                            gap-2
+                            font-thin
+                            p-5
+                        "
+                >
+                    
+                    <Link href={'/'}>
+                        <a className="group">
+                            <div
+                                className="
+                            text-5xl
+                            font-sans
+                            text-amber-100
+                            text-opacity-50
+                            group-hover:text-amber-400
+                            group-hover:text-opacity-100
+                            "
+                            >
+                            Paul Lee
+                            </div>
+                        </a>
+                    </Link>
+                    <div
+                        className='
+                        self-center md:self-end
+                        flex
+                        gap-4 md:gap-2
+                        text-3xl med:text-4xl
+                        text-amber-100
+                        text-opacity-50
+                        '
+                    >
+                    <MenuLink
+                        href="/bio"
+                        className='
+                        group-hover:text-amber-500
+                        group-hover:text-opacity-100'
+                        iconComponent={Mail}
+                        iconText="contact"
+                        iconTextStyling='
+                        text-xs
+                        visible md:invisible group-hover:visible'
+                    
+                    />
 
+                    </div>
 
-        </div>
+                </div>
+            </div>
+
+    </div>
     )
 }
 
